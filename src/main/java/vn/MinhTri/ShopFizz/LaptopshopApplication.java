46b7540b7255a8d9f778b4840730ff1509582f8d
Hoang-Minh-Tri <minhtri4869it@gmail.com>
2024-10-10 22:26:15 +0700

    Tạo dự án mới

---
+++ src/main/java/vn/MinhTri/ShopFizz/LaptopshopApplication.java
@@ -0,0 +1,14 @@
+package vn.MinhTri.ShopFizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopshopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LaptopshopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/vn/MinhTri/ShopFizz/services/MyService.java
@@ -0,0 +1,8 @@
+package vn.MinhTri.ShopFizz.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+}
