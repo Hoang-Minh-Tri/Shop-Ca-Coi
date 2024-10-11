1edd5b3b5ad284ea4ac81915907ec4aa540c1b2a
Hoang-Minh-Tri <minhtri4869it@gmail.com>
2024-10-11 08:16:59 +0700

    Tạo giao diện tạo mới người dùng

--- src/main/java/vn/MinhTri/ShopFizz/controller/UserController.java
+++ src/main/java/vn/MinhTri/ShopFizz/controller/UserController.java
@@ -1,9 +1,15 @@
 package vn.MinhTri.ShopFizz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import vn.MinhTri.ShopFizz.domain.User;
 import vn.MinhTri.ShopFizz.services.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
@@ -18,4 +24,25 @@ public class UserController {
     public String getHomePage() {
         return "hello";
     }
+
+    @GetMapping("/admin/user")
+    public String getMethodName(Model model) {
+        User user = new User();
+        user.setId(1);
+        user.setEmail("minhtri@gmail.com");
+        user.setFullName("minhtri");
+        user.setPassword("123456");
+        user.setAddress("Ha Noi");
+        user.setPhone("123456");
+        model.addAttribute("newUser", user);
+        return "admin/user/create";
+    }
+
+    @PostMapping("/admin/user/create1")
+    public String postCreateUser(@ModelAttribute("newUser") User user) {
+        user.ToString();
+
+        return "redirect:/";
+    }
+
 }
